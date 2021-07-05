f644c809638f9965749aed544d48b17d18fc16bb
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-05 17:04:56 -0600

    updating new file

--- jobs/new_dc.py
+++ jobs/new_dc.py
@@ -1,6 +1,19 @@
-class NewDataCenter(Job, FormEntry):
-    """Job to build out a new Datacenter"""
+from django.utils.text import slugify
 
-    region = FormEntry.region
-    site_name = FormEntry.site
-    rack = FormEntry.rack
+from nautobot.dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Site, Region, Racks
+from nautobot.extras.models import Status
+from nautobot.extras.jobs import *
+
+
+class NewDC(Job):
+
+    class Meta:
+        name = "New DataCenter"
+        description = "Build new vxlan deployment"
+        field_order = ['region', 'site_name', 'relay_rack', 'underlay_p2p_network_summary', 'overlay_loopback_network_summary', 'vtep_loopback_network_summary', 'mlag_leaf_peer_l3', 'mlag_peer', '_peer_groups', 'spine_switch_count', 'spine_bgp_as', 'leaf_bgp_as_range', 'leaf_switch_count', 'tor_switch_count']
+
+    region = ObjectVar(
+        description="Choose Region",
+        model=Region,
+        required=False
+    )
