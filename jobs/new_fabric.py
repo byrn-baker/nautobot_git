c1d87e9be7592fc467fac75379c376797f587bfb
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-05 11:54:11 -0600

    update

--- jobs/new_fabric.py
+++ jobs/new_fabric.py
@@ -10,7 +10,7 @@ class NewDC(Job):
     class Meta:
         name = "New DataCenter"
         description = "Build new vxlan deployment"
-        field_order = ['region', 'fabric_name', 'relay_rack', 'underlay_p2p_network_summary', 'overlay_loopback_network_summary', 'vtep_loopback_network_summary', 'mlag_leaf_peer_l3', 'mlag_peer', 'vxlan_vlan_aware_bundles', 'bgp_peer_groups', 'spine_switch_count', 'spine_bgp_as', 'leaf_bgp_as_range', 'leaf_switch_count', 'tor_switch_count']
+        field_order = ['region', 'site_name', 'relay_rack', 'underlay_p2p_network_summary', 'overlay_loopback_network_summary', 'vtep_loopback_network_summary', 'mlag_leaf_peer_l3', 'mlag_peer', 'vxlan_vlan_aware_bundles', 'bgp_peer_groups', 'spine_switch_count', 'spine_bgp_as', 'leaf_bgp_as_range', 'leaf_switch_count', 'tor_switch_count']
 
     region = ObjectVar(
         description="Choose Region",
@@ -30,21 +30,21 @@ class NewDC(Job):
         description = "Underlay P2P network - Assign range larger then total [spines * total potential leafs * 2"
     )
 
-    # overlay_loopback_network_summary = ipaddress.ip_network(
-    #     description = "Overlay Loopback network - Assign range larger then total spines + total leafs switches"
-    # )
+    overlay_loopback_network_summary = StringVar(
+        description = "Overlay Loopback network - Assign range larger then total spines + total leafs switches"
+    )
 
-    # vtep_loopback_network_summary = ipaddress.ip_network(
-    #     description = "Vtep Loopback network - Assign range larger then total leaf switches"
-    # )
+    vtep_loopback_network_summary = StringVar(
+        description = "Vtep Loopback network - Assign range larger then total leaf switches"
+    )
 
-    # mlag_leaf_peer_l3 = ipaddress.ip_network(
-    #     description = "Leaf L3 MLAG network - Assign range larger then total spines + total leafs switches"
-    # )
+    mlag_leaf_peer_l3 = StringVar(
+        description = "Leaf L3 MLAG network - Assign range larger then total spines + total leafs switches"
+    )
 
-    # mlag_peer = ipaddress.ip_network(
-    #     description = "MLAG Peer network - Assign range larger then total spines + total leafs switches"
-    # )
+    mlag_peer = StringVar(
+        description = "MLAG Peer network - Assign range larger then total spines + total leafs switches"
+    )
 
     # vxlan_vlan_aware_bundles = Bool(
     #     description = "Should bundles be vxlan vlan aware?"
