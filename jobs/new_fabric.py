56e48e08ef5a87319585ec142682441324323755
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-05 12:33:43 -0600

    update

--- jobs/new_fabric.py
+++ jobs/new_fabric.py
@@ -10,7 +10,7 @@ class NewDC(Job):
     class Meta:
         name = "New DataCenter"
         description = "Build new vxlan deployment"
-        field_order = ['region', 'site_name', 'relay_rack', 'underlay_p2p_network_summary', 'overlay_loopback_network_summary', 'vtep_loopback_network_summary', 'mlag_leaf_peer_l3', 'mlag_peer', 'vxlan_vlan_aware_bundles', 'bgp_peer_groups', 'spine_switch_count', 'spine_bgp_as', 'leaf_bgp_as_range', 'leaf_switch_count', 'tor_switch_count']
+        field_order = ['region', 'site_name', 'relay_rack', 'underlay_p2p_network_summary', 'overlay_loopback_network_summary', 'vtep_loopback_network_summary', 'mlag_leaf_peer_l3', 'mlag_peer', '_peer_groups', 'spine_switch_count', 'spine_bgp_as', 'leaf_bgp_as_range', 'leaf_switch_count', 'tor_switch_count']
 
     region = ObjectVar(
         description="Choose Region",
@@ -46,9 +46,9 @@ class NewDC(Job):
         description = "MLAG Peer network - Assign range larger then total spines + total leafs switches"
     )
 
-    # bgp_peer_groups = Stringvar(
-    #     description = "List the names of th BGP Peer Groups - Comma seperated 'IPv4_UNDERLAY_PEERS', 'EVPN_OVERLAY_PEERS', 'MLAG_IPv4_UNDERLAY_PEER' "
-    # )
+    _peer_groups = Stringvar(
+        description = "List the names of th BGP Peer Groups - Comma seperated 'IPv4_UNDERLAY_PEERS', 'EVPN_OVERLAY_PEERS', 'MLAG_IPv4_UNDERLAY_PEER' "
+    )
 
     spine_switch_count = IntegerVar(
         description = "Number of Spines to be deployed"
