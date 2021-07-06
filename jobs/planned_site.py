67787f95cb04624e0407c1b620a704fc97e5b2da
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-06 11:25:12 -0600

    update

--- jobs/planned_site.py
+++ jobs/planned_site.py
@@ -1,6 +1,6 @@
 from django.utils.text import slugify
 
-from nautobot.dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Site
+from nautobot.dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Site, Region, Racks
 from nautobot.extras.models import Status
 from nautobot.extras.jobs import *
 
@@ -10,7 +10,13 @@ class NewBranch(Job):
     class Meta:
         name = "New Pod"
         description = "Provision a new pod"
-        field_order = ['site_name', 'site_asn','manufacturer', 'switch_model', 'router_count', 'router_model', 'core_switch_count', 'access_switch_count']
+        field_order = ['region', 'site_name', 'relay_rack', 'underlay_p2p_network_summary', 'overlay_loopback_network_summary', 'vtep_loopback_network_summary', 'mlag_leaf_peer_l3', 'mlag_peer', '_peer_groups', 'spine_switch_count', 'spine_bgp_as', 'leaf_bgp_as_range', 'leaf_switch_count', 'tor_switch_count']
+    
+    region = ObjectVar(
+        description="Choose Region",
+        model=Region,
+        required=False
+    )
 
     site_name = StringVar(
         description="Name of the new pod"
