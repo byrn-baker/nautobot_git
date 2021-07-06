4f0fb154da16ded2616af1a57f8f0bffb2fba265
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-06 11:39:36 -0600

    update

--- jobs/provision_datacenter.py
+++ jobs/provision_datacenter.py
@@ -10,16 +10,15 @@ class DataCenter(Job):
         description = "Build a new DataCenter with VXLAN"
         field_order = ['region', 'site_name', 'relay_rack', 'underlay_p2p_network_summary', 'overlay_loopback_network_summary', 'vtep_loopback_network_summary', 'mlag_leaf_peer_l3', 'mlag_peer', '_peer_groups', 'spine_switch_count', 'spine_bgp_as', 'leaf_bgp_as_range', 'leaf_switch_count', 'tor_switch_count']
 
-    region = ObjectVar(
-        description="Choose Region",
-        model=Region
-    )
-
     site_name = StringVar(
         description = "Name for the new fabric"
     )
 
-    # relay_rack = IntegerVar(
-    #     description = "Choice how many Relay Racks"
-    # )
+    relay_rack = IntegerVar(
+        description = "Choice how many Relay Racks"
+    )
 
+      region = ObjectVar(
+        description="Choose Region",
+        model=Region
+    )
