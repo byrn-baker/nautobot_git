926a749d0c4488d72dee0a7001d044faf58e8d52
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-06 13:15:21 -0600

    update

--- jobs/provision_datacenter.py
+++ jobs/provision_datacenter.py
@@ -125,7 +125,7 @@ class DataCenter(Job):
         underlay_pfx = Prefix(
             prefix=data['underlay_p2p_network_summary'],
             site=site,
-            status='active'
+            status='Active'
         )
         underlay_pfx.validated_save()
         self.log_success(obj=underlay_pfx, message="Created new underlay prefix")
