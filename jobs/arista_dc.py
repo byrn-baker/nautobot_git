40dcd5a38d0da7e49a64f4879dbe1a286534ac17
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-23 17:38:07 -0600

    update

--- jobs/arista_dc.py
+++ jobs/arista_dc.py
@@ -451,12 +451,12 @@ class CreateAristaPod(Job):
                     )
                     self.log_success(obj=intf_name, message=f"{intf_name} successfully created on {device_name}")
 
-                    ethernet1 = Interface.objects.get(name="Ethernet1", device=device_name)
+                    ethernet1 = Interface.objects.get(name="Ethernet1", device=device)
                     ethernet1.type = "lag"
                     ethernet1.mode = "tagged-all"
                     ethernet1.mtu = 1500
 
-                    ethernet2 = Interface.objects.get(name="ethernet2", device=device_name)
+                    ethernet2 = Interface.objects.get(name="ethernet2", device=device)
                     ethernet2.type = "lag"
                     ethernet2.mode = "tagged-all"
                     ethernet2.mtu = 1500
