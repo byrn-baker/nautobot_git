7d4294e435af13e2c65cde46e3ebfbd819efa555
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-24 11:50:07 -0600

    update

--- jobs/arista_dc.py
+++ jobs/arista_dc.py
@@ -503,7 +503,7 @@ class CreateAristaPod(Job):
                             device=device, 
                     )
                     self.log_success(obj=intf_name, message=f"{intf_name} successfully created on {device_name}")
-                    if "mode" in iface.keys:
+                    if "mode" in iface.keys():
                         intf_name.mode = iface["mode"]
 
 
