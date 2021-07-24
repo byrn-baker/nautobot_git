6026da8c4238725a919caff1ee7f6f788e4f836c
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-23 20:49:00 -0600

    update

--- jobs/arista_dc.py
+++ jobs/arista_dc.py
@@ -453,7 +453,7 @@ class CreateAristaPod(Job):
                                 mtu=1500,
                                 device=device,
                             )
-                            intf_name.lag
+
                         elif iface =='Ethernet2':
                             portchannel = Interface.objects.get(name="Port-Channel10", device=device)
                             intf_name = Interface.objects.get_or_create(
