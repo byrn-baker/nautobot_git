4b2d9dcc059e548363d090fd73c6694d3cc2cec7
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-25 15:51:41 -0600

    update

--- jobs/arista_dc.py
+++ jobs/arista_dc.py
@@ -586,7 +586,7 @@ class CreateAristaPod(Job):
                         ip = IPAddress.objects.create('192.168.255.2/30', assigned_object=interface)
                         self.log_success(message=f"Created MLAG PEER address on {interface.device.name}::{interface}")
 
-                    if device == f"{dc_code}-leaf-03":
+                    elif device == f"{dc_code}-leaf-03":
                         interface = Interface.objects.get(name="Vlan4094", device=device)
                         ip = IPAddress.objects.create('192.168.255.1/30', assigned_object=interface)
                         self.log_success(message=f"Created MLAG PEER address on {interface.device.name}::{interface}")
