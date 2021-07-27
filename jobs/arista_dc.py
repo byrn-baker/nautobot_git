542c5b8fac67d9868cb96faeff4494708520a3bd
Byrn Baker <63813461+TwistByrn@users.noreply.github.com>
2021-07-27 07:34:50 -0600

    update

--- jobs/arista_dc.py
+++ jobs/arista_dc.py
@@ -709,7 +709,7 @@ class CreateAristaPod(Job):
         #######################################
         for role, data in ROLES.items():
             for i in range(1, data.get("nbr", 2) + 1):
-                device_name = f"{dc_code}-{role}-{i:02}"
+                device_name = f"{role}{i}-{dc_code}"
                 device = Device.objects.get(name=device_name)
                 dev_name = device_name.replace(f"-{dc_code}","")
 
